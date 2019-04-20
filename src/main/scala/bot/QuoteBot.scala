a7bed2420030b4794fcbe200f8482b40a73c0dee
Laurin Novak <laurin.novak@firstbird.com>
2019-04-20 17:13:17 +0300

    add basic quote command

---
+++ src/main/scala/bot/QuoteBot.scala
@@ -0,0 +1,23 @@
+package bot
+
+import com.bot4s.telegram.api.declarative.Commands
+import com.bot4s.telegram.api.{Polling, RequestHandler, TelegramBot}
+import com.bot4s.telegram.clients.ScalajHttpClient
+
+import scala.io.Source
+
+class QuoteBot extends TelegramBot with Polling with Commands {
+  override val client: RequestHandler = new ScalajHttpClient(token)
+
+  onCommand("/start") { implicit msg =>
+    reply("Get to da choppa!\nUse /arnold to get a Schwarzenegger quote.")
+  }
+
+  onCommand("/arnold") { implicit msg =>
+    reply("Come with me, if you want to lift")
+  }
+
+  private lazy val token = scala.util.Properties
+    .envOrNone("BOT_TOKEN")
+    .getOrElse(Source.fromFile("bot.token").getLines().mkString)
+}
